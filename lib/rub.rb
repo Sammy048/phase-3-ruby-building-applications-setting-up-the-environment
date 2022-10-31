0561a80861754597621cab0f0f8fb4774fce83ab
sammy-coder <sammymasib000@gmail.com>
2022-10-31 21:52:27 +0300

    Completed assignment

---
+++ lib/rub.rb
@@ -0,0 +1,11 @@
+require_relative '../config/environment'
+
+lawn = Garden.new(name: 'Front Lawn')
+
+basil = Plant.new(name: 'Basil')
+basil.garden = lawn
+
+cucumber = Plant.new(name: 'Cucumber')
+cucumber.garden = lawn
+
+p lawn.plants
